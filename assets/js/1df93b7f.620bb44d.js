669e1d0720297de6e35d3bd0675ac689d6117b71
Mech <mechsix@users.noreply.github.com>
2021-09-30 01:22:06 +0800

    Deploy website - based on afc8d4759fe5da03435854c8334b368ad2259b9d

---
+++ assets/js/1df93b7f.620bb44d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmechsix_github_io_source=self.webpackChunkmechsix_github_io_source||[]).push([[237],{9754:function(e,t,i){i.r(t);var c=i(7294),u=i(6698),n=i(2263);t.default=function(){var e=(0,n.Z)().siteConfig;return c.createElement(u.Z,{title:""+e.title,description:"Sample Desc"},c.createElement("div",null,"Portal page"))}}}]);
