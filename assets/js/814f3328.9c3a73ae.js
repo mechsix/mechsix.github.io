1ec6fd79c3d1c1ca64aae54dc590a2883757f2ff
Mech <mechsix@users.noreply.github.com>
2021-11-02 01:16:05 +0800

    Deploy website - based on 4d5708c66d0d6a952f8e30cda2d3e3879970d090

---
+++ assets/js/814f3328.9c3a73ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmechsix_github_io_source=self.webpackChunkmechsix_github_io_source||[]).push([[2535],{45641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Portal","permalink":"/blog/welcome"}]}')}}]);
